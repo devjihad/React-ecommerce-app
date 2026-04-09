b10d7d18b50325fce79a512100f446125840ee3b
devjihad <mdhajihad609@gmail.com>
2026-04-09 06:42:16 +0600

    creating reusable title component with responsive

--- src/Component/Home/LatestProduct/AllPruduct.jsx
+++ src/Component/Home/LatestProduct/AllPruduct.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { data, Link } from 'react-router-dom';
 import { Data } from '../../Contaxt/Contaxt';
+import Title from '../Title';
+
 
 const AllPruduct = () => {
     const {products} = useContext(Data)
@@ -12,8 +14,9 @@ const AllPruduct = () => {
     console.log(value)
     return (
         <div>
-            <h3 className='text-[38px] font-semibold font-playfair text-center mt-16 ' >Latest Product </h3>
-            <p className='text-[14px] font-semibold text-gray-600 text-center mt-1 mb-10'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat nostrum culpa est totam ex nemo.</p>
+            {/* <h3 className='text-[38px] font-semibold font-playfair text-center mt-16 ' >Latest Product </h3>
+            <p className='text-[14px] font-semibold text-gray-600 text-center mt-1 mb-10'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat nostrum culpa est totam ex nemo.</p> */}
+            <Title value1={'Latest'} value2={'Product'} value={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat nostrum culpa est totam ex nemo.'}/>
             <div className='grid gap-3 gap-y-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
                 {
                     value.map((item, index)=>(
---
+++ src/Component/Home/Title.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Title = ({value1, value2 , value}) => {
+    return (
+        <div className='text-center mt-10 mb-10'>
+           <div className='flex gap-1 items-center justify-center'>
+                 <h3 className='text-[38px] font-semibold font-playfair  '>{value1} <span className='text-gray-700'>{value2}</span></h3>
+                <hr className='h-0.5 border-0 bg-gray-500 w-8 rounded-lg'/>
+           </div>
+           <p className='text-[14px] text-gray-600 '>{value}</p>
+            
+        </div>
+    );
+};
+
+export default Title;
