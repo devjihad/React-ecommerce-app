704e68b069df65dd69de71f863beec74728b8d3d
devjihad <mdhajihad609@gmail.com>
2026-04-11 19:46:05 +0600

    category section

--- src/Component/Collection/Collection.jsx
+++ src/Component/Collection/Collection.jsx
@@ -2,11 +2,22 @@ import React from "react";
 
 const Collection = () => {
   return (
-    <div className="container ">
-      <div>
+    <div className="container flex flex-col sm:flex-row gap-8 ">
+      <div className="min-w-60">
         <h3>Filter </h3>
-        <div>
-          <input type="text" name="" id="" />
+        <div className={`border border-gray-300 py-5 px-3 mt-6 `}>
+          <p className="text-sm font-semibold mb-3">CATEGORYS</p>
+          <div className="text-sm">
+            <p className="flex gap-2 items-center">
+              <input type="checkbox" name="" id="" value={'Men'} />Men
+            </p>
+            <p className="flex gap-2 items-center">
+              <input type="checkbox" name="" id="" value={'Women'} />Women
+            </p>
+            <p className="flex gap-2 items-center">
+              <input type="checkbox" name="" id="" value={'Kids'} />Kids
+            </p>
+          </div>
         </div>
       </div>
     </div>
